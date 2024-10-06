b54e04421d9f5126a4c4fe4c753282bcccfd8c2c
Emil Paulsen <emil.paulsen@gmail.com>
2024-10-06 20:53:44 +0200

    Add vite to frontend

--- backend/Program.fs
+++ backend/Program.fs
@@ -10,16 +10,17 @@ open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 
-type Event = { id: int; name: string; date: string; time: string }
+type Event =
+    { id: int
+      name: string
+      date: string
+      time: string }
 
 // ---------------------------------
 // Models
 // ---------------------------------
 
-type Message =
-    {
-        Text : string
-    }
+type Message = { Text: string }
 
 // ---------------------------------
 // Views
@@ -28,58 +29,46 @@ type Message =
 module Views =
     open Giraffe.ViewEngine
 
-    let layout (content: XmlNode list) =
-        html [] [
-            head [] [
-                title []  [ encodedText "FamilyEventsBackend" ]
-                link [ _rel  "stylesheet"
-                       _type "text/css"
-                       _href "/main.css" ]
-            ]
-            body [] content
-        ]
-
-    let partial () =
-        h1 [] [ encodedText "FamilyEventsBackend" ]
-
-    let index (model : Message) =
-        [
-            partial()
-            p [] [ encodedText model.Text ]
-        ] |> layout
+    let indexPage: XmlNode =
+        html
+            []
+            [ head
+                  []
+                  [ meta [ _charset "UTF-8" ]
+                    meta [ _name "viewport"; _content "width=device-width, initial-scale=1.0" ]
+                    title [] [ encodedText "Family Events" ] ]
+              body [] [ div [ _id "elm" ] []; script [ _type "module"; _src "/src/Main.elm" ] [] ] ]
 
 // ---------------------------------
 // Web app
 // ---------------------------------
 
-let indexHandler (name : string) =
-    let greetings = sprintf "Hello %s, from Giraffe!" name
-    let model     = { Text = greetings }
-    let view      = Views.index model
-    htmlView view
+let indexHandler = htmlView Views.indexPage
 
 let getEventsHandler: HttpHandler =
-    let events = [
-        { id = 1; name = "Family Dinner"; date = "2024-10-02"; time = "19:00" }
-        { id = 2; name = "Kids Soccer Practice"; date = "2024-10-03"; time = "17:30" }
-    ]
+    let events =
+        [ { id = 1
+            name = "Family Dinner"
+            date = "2024-10-02"
+            time = "19:00" }
+          { id = 2
+            name = "Kids Soccer Practice"
+            date = "2024-10-03"
+            time = "17:30" } ]
+
     json events
 
 let webApp =
-    choose [
-        GET >=>
-            choose [
-                route "/" >=> indexHandler "world"
-                route "/events" >=> getEventsHandler
-                routef "/hello/%s" indexHandler
-            ]
-        setStatusCode 404 >=> text "Not Found" ]
+    choose
+        [ GET
+          >=> choose [ route "/" >=> indexHandler; route "/events" >=> getEventsHandler ]
+          setStatusCode 404 >=> text "Not Found" ]
 
 // ---------------------------------
 // Error handler
 // ---------------------------------
 
-let errorHandler (ex : Exception) (logger : ILogger) =
+let errorHandler (ex: Exception) (logger: ILogger) =
     logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
     clearResponse >=> setStatusCode 500 >=> text ex.Message
 
@@ -87,50 +76,51 @@ let errorHandler (ex : Exception) (logger : ILogger) =
 // Config and Main
 // ---------------------------------
 
-let configureCors (builder : CorsPolicyBuilder) =
+let configureCors (builder: CorsPolicyBuilder) =
     builder
         .WithOrigins(
+            "http://localhost:5173",
             "http://localhost:8000",
             "http://localhost:5000",
-            "https://localhost:5001")
-       .AllowAnyMethod()
-       .AllowAnyHeader()
-       |> ignore
+            "https://localhost:5001"
+        )
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+    |> ignore
 
-let configureApp (app : IApplicationBuilder) =
+let configureApp (app: IApplicationBuilder) =
     let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
+
     (match env.IsDevelopment() with
-    | true  ->
-        app.UseDeveloperExceptionPage()
-    | false ->
-        app .UseGiraffeErrorHandler(errorHandler)
-            .UseHttpsRedirection())
+     | true -> app.UseDeveloperExceptionPage()
+     | false -> app.UseGiraffeErrorHandler(errorHandler).UseHttpsRedirection())
         .UseCors(configureCors)
         .UseStaticFiles()
         .UseGiraffe(webApp)
 
-let configureServices (services : IServiceCollection) =
-    services.AddCors()    |> ignore
+let configureServices (services: IServiceCollection) =
+    services.AddCors() |> ignore
     services.AddGiraffe() |> ignore
 
-let configureLogging (builder : ILoggingBuilder) =
-    builder.AddConsole()
-           .AddDebug() |> ignore
+let configureLogging (builder: ILoggingBuilder) =
+    builder.AddConsole().AddDebug() |> ignore
 
 [<EntryPoint>]
 let main args =
     let contentRoot = Directory.GetCurrentDirectory()
-    let webRoot     = Path.Combine(contentRoot, "WebRoot")
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(
-            fun webHostBuilder ->
-                webHostBuilder
-                    .UseContentRoot(contentRoot)
-                    .UseWebRoot(webRoot)
-                    .Configure(Action<IApplicationBuilder> configureApp)
-                    .ConfigureServices(configureServices)
-                    .ConfigureLogging(configureLogging)
-                    |> ignore)
+    let webRoot = Path.Combine(contentRoot, "WebRoot")
+
+    Host
+        .CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(fun webHostBuilder ->
+            webHostBuilder
+                .UseContentRoot(contentRoot)
+                .UseWebRoot(webRoot)
+                .Configure(Action<IApplicationBuilder> configureApp)
+                .ConfigureServices(configureServices)
+                .ConfigureLogging(configureLogging)
+            |> ignore)
         .Build()
         .Run()
+
     0
