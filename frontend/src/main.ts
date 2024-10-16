0d3e7167461ef2d5cbb59f4c49201682cc901341
Emil Paulsen <emil.paulsen@gmail.com>
2024-10-16 21:10:33 +0200

    Api host logging

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,8 @@ import { Elm } from "./Main.elm";
 
 const apiHost = import.meta.env.API_HOST || "http://localhost:5000";
 
+console.log("API_HOST", apiHost);
+
 Elm.Main.init({
   node: document.querySelector("#app"),
   flags: apiHost,
