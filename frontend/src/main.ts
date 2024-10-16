7403ed5bfb46d53b25d9a22006bd8ceb33629af4
Emil Paulsen <emil.paulsen@gmail.com>
2024-10-16 21:15:33 +0200

    Rename api host setting

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,6 @@
 import { Elm } from "./Main.elm";
 
-const apiHost = import.meta.env.API_HOST || "http://localhost:5000";
-
-console.log("API_HOST", apiHost);
+const apiHost = import.meta.env.VITE_API_HOST || "http://localhost:5000";
 
 Elm.Main.init({
   node: document.querySelector("#app"),
